fec712b4cbfe64ddc38720bdd4b9c2de1264219f
heloint <majerdaniel93@gmail.com>
2023-03-03 13:46:26 +0100

    all DONE; BEFORE css and html purging, reviewing.

--- website/js/index.script.js
+++ website/js/index.script.js
@@ -1,27 +1,29 @@
 const nav = $(".navbar");
 
 $(document).ready(function () {
+  // Apply navbar background color by default,
+  // if at start up the screen condition is met.
   if ($(window).width() < 991) {
     nav.addClass("collasped-nav");
   } else {
     nav.removeClass("collasped-nav");
   }
 
+  // Apply navbar background color on scroll down,
+  // if at start up the screen condition is met.
   $(function () {
     $(document).scroll(function () {
       nav.toggleClass("scrolled", $(this).scrollTop() > nav.height());
     });
   });
 
+  // Apply navbar background color on size change,
+  // if at start up the screen condition is met.
   $(window).on("resize", function () {
     if ($(this).width() < 991) {
       nav.addClass("collasped-nav");
     } else {
       nav.removeClass("collasped-nav");
     }
-  });
-
-  $(".learn-more-btn").click(function () {
-    $("#modal").modal("show");
   });
 });
